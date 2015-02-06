48ce0bd00d2202e5c8ea41507a7f59d568c9014b
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-02-05 23:03:51 -0800

    finished video 18 , no comments {Building EnemyCreep Class}

--- js/game.js
+++ js/game.js
@@ -46,6 +46,7 @@ var game = {
 		//registering the entities // both bases
 		me.pool.register("PlayerBase", game.PlayerBaseEntity);
 		me.pool.register("EnemyBase", game.EnemyBaseEntity);
+		me.pool.register("EnemyCreep", game.EnemyCreep, true);
 
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
