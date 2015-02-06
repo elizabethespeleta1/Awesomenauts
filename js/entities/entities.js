09d1956eca282bef65d07d22b6ef4fc57025d246
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-02-05 20:49:03 -0800

    commented on code for video 14 {Setting up Collisions}

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -55,8 +55,8 @@ game.PlayerEntity = me.Entity.extend({
 		}
 		//if you stop pressing the right key
 		else if (me.input.isKeyPressed("left")){
-			this.facing = "left";
 			this.body.vel.x -=this.body.accel.x * me.timer.tick;
+			this.facing = "left";
 			this.flipX(false);
 		}
 		else{
@@ -64,7 +64,9 @@ game.PlayerEntity = me.Entity.extend({
 			this.body.vel.x = 0;
 		}
 
+		//if you press jump && make sure you dont double jumping // your not already jumping and your not falling 
 		if(me.input.isKeyPressed("jump") && !this.body.falling && !this.body.jumping){
+			//if it runs you can jump
 			this.body.jumping = true;
 			this.body.vel.y -= this.body.accel.y * me.timer.tick;
 		} 
